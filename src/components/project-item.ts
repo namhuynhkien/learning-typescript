d83ee5380e953ed3dfbbc8a62160020277775509
Nam Huynh <namhuynhkien@gmail.com>
2021-01-13 22:41:13 +0700

    Webpack lectures finished

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Draggable } from '../models/drag-drop.js';
-import Cmp from './base-component.js';
-import { Autobind } from '../decorators/autobind.js';
-import { Project } from '../models/project.js';
+import { Draggable } from '../models/drag-drop';
+import Cmp from './base-component';
+import { Autobind } from '../decorators/autobind';
+import { Project } from '../models/project';
 
 export class ProjectItem
   extends Cmp<HTMLUListElement, HTMLLIElement>
--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import Cmp from './base-component.js';
-import { Autobind } from '../decorators/autobind.js';
-import { Project, ProjectStatus } from '../models/project.js';
-import { DragTarget } from '../models/drag-drop.js';
-import { projectState } from '../state/project-state.js';
-import { ProjectItem } from './project-item.js';
+import Cmp from './base-component';
+import { Autobind } from '../decorators/autobind';
+import { Project, ProjectStatus } from '../models/project';
+import { DragTarget } from '../models/drag-drop';
+import { projectState } from '../state/project-state';
+import { ProjectItem } from './project-item';
 
 export class ProjectList
   extends Cmp<HTMLDivElement, HTMLElement>
